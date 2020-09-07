0d8e489db90d054200534969dc627c216fb5f75a
Vivek Sengar <vivek.sengar3112@gmail.com>
2020-09-08 00:55:28 +0530

    adding asp webforms

---
+++ ASP.Net Folder/login.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class login : System.Web.UI.Page
+{
+    private const string Value = "function was called";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void loginButton_Click(object sender, EventArgs e)
+    {
+        Console.Write(Value);
+        Response.Write("Button Clicked");
+    }
+}
