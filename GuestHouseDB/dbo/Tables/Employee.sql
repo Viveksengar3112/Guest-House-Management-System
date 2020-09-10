8902a31b58e7de8309ec49a287725d0f9994c931
Vivek Sengar <31692761+Viveksengar3112@users.noreply.github.com>
2020-09-10 22:35:45 +0530

    Revert "Back end initial commit"

--- GuestHouseDB/dbo/Tables/Employee.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Employee] (
-    [EmployeeID]   INT            NOT NULL,
-    [FirstName]    NVARCHAR (50)  NOT NULL,
-    [LastName]     NVARCHAR (50)  NOT NULL,
-    [EmailAddress] NVARCHAR (100) NOT NULL,
-    [MobileNo]     INT            NULL,
-    PRIMARY KEY CLUSTERED ([EmployeeID] ASC)
-);
-
